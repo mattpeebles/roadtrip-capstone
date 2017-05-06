2146fdedc30f11fb007abd06e976c758ee5e1d47
MattPeebles <peebles.mm@gmail.com>
2017-05-06 16:23:09 -0500

    Wrote initial js framework for directions
    A variety of changes were implemented. Moved original app.js to legacy.js
    because code was tested in the original file. App.js now contains the
    beginning implementation of my low level design. Specifically: autocomplete
    for beginning and end points, initiating destination array; leg array;
    legData object; and legcounter(for navigation), grabbing user entered
    destinations and pushing them to an array, grabbing destinations two
    at a time to determine the leg of the trip,  adding additional
    destinations via button, calculating geocoordinates for the leg of a
    journey, pushing geocoordinates received by Google Geocoordinate API
    to their respective key in the legdata object, and calling data when
    user clicks the submit button.

--- app.js
+++ app.js
@@ -1,64 +1,118 @@
-// GoogleMaps Apikey =  AIzaSyCMCPU7PyFM8_7KihOTm3T_cfitx-494cQ 
+// State
+var destinations = [];
 
-// GoogleMaps API Call
+var addDestCounter = 1;
 
-var EVENTBRITE_BASE_URL = "https://www.eventbriteapi.com/v3/events/search/"
+var leg = [];
 
-// allows for additional destinations
+var autocompleteArray = [];
+
+var legCounter = 0;
+
+var legData  = {startPoint: {geocode: {latitude: null,
+                                      longitude: null},
+                            weather: {}
+                           },
+               endPoint: {geocode: {latitude: null,
+                                    longitude: null},
+                          weather: {}
+                         },
+               midPoint: {geocode: {latitude: null,
+                                    longitude: null},
+                          weather: {}
+                          },
+
+               "0.5point": {geocode: {latitude: null,
+                                    longitude: null},
+                          weather: {}
+                         },
+               "1.5point": {geocode: {latitude: null,
+                                    longitude: null},
+                          weather: {}
+                         },
+              };
+
+// State Manipulation Section
+
+  // grabs all user inputted destinations and pushes
+  //them to the destinations array
+function getDestinations(){
+  destinations = [];
+  $("#destination-form .js-destination").each(function(){
+    destinations.push($(this).val());
+  })
+  getLeg(); //initiates initial leg of the journey
+}
+
+  //this function gets the leg of the journey.
+  //it will always be two destinations
+  //this will be called once we start navigating
+function getLeg(){
+  leg = [destinations[legCounter], destinations[legCounter + 1]];
+}
+
+  //Obtaining geocodes section
+  var GOOGLE_GEOCODE_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json";
+  var myGeoArray = []; //holds coordinates that geocode returns
+
+function getDataFromGoogleGeocode(address, callback){
+    var query = {
+      "address": address,
+      key: "AIzaSyCMCPU7PyFM8_7KihOTm3T_cfitx-494cQ",
+    };
+    $.getJSON(GOOGLE_GEOCODE_BASE_URL, query, callback)
+}
+
+function returnGeocodeData(data){
+      myGeoArray.push(data.results[0]["geometry"]["location"]);
+        legData["startPoint"]["geocode"] = myGeoArray[0];
+        legData["endPoint"]["geocode"] = myGeoArray[1];
+}
+
+function updateLegDataGeocode(){
+    myGeoArray=[];
+    leg.forEach(function(item){
+      getDataFromGoogleGeocode(item, returnGeocodeData);
+    });
+}
+
+
+// DOM Manipulation
+  // adds new destination input area in form
 function addDest(){
-	var counter = 1;
-	$("#destination-form").on("click", "#js-addDest", function(event){
-		event.preventDefault();
-		var newDestForm = "<br><input type=\"text\" name=\"dest-" + counter + "\" placeholder=\"Destination-" +counter+ "\"><input type=\"text\" name=\"Dest-" +counter+ "-date\" placeholder=\"length of stay\"><button id=\"js-addDest\">Add Destination</button>";
-		$(this).after(newDestForm);
-		counter++;
-	})
-};
-
-
-//Route between start and end destinations
-
-function routeDest(){
-	var resultElement = "";
-	$("#js-form-submit").on("click", function(event){
-		event.preventDefault();
-		var start = $("#start").val();
-		start = start.replace(/ /g, "%20");
-		var end = $("#end").val();
-		end = end.replace(" ", "%20");
-		resultElement += "<iframe width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0\" src=\"https://www.google.com/maps/embed/v1/directions?"+
-			"origin=" + start + 
-			"&destination=" + end + 
-			"&key=AIzaSyCMCPU7PyFM8_7KihOTm3T_cfitx-494cQ\"" +
-			"allowfullscreen></iframe>";
-		$("#map-holder").html(resultElement);
-	})
+  $("#destination-form").on("click", "#js-addDest", function(event){
+    event.preventDefault();
+    var newDestForm = "<br><input type=\"text\" name=\"dest-" + addDestCounter + "\" id = \"destination-" + addDestCounter + "\" class=\"js-destination\" placeholder=\"Destination\">" + 
+    "<input type=\"text\" name=\"Dest-" +addDestCounter+ "-date\" placeholder=\"length of stay\">" + 
+    "<button id=\"js-addDest\">Add Destination</button>";
+    $(this).after(newDestForm);
+    addAutoComplete();
+    addDestCounter++;
+  })
 }
 
-function getDataFromEventBrite(callback){
-	var query = {
-		"location.latitude": 29.7604267, //need to determine how to get latitude of inputted city
-		"location.longitude": -95.3698028, //need to determine how to get longitude
-		"start_date.range_start": $("#start-date").val() + "T13:00:00",
-		token: "NYUIK7WAP7JD57IF4W4H",
-	}
-	$.getJSON(EVENTBRITE_BASE_URL, query, callback)
+  //Google Autocomplete API Section
+  //NEED TO DETERMINE HOW TO ADD FUNCTIONALITY
+  //TO NEW DESTINATIONS
+var begin = document.getElementById("start");
+var end = document.getElementById("end")
+
+var autocompleteBegin = new google.maps.places.Autocomplete(begin)
+var autocompleteEnd = new google.maps.places.Autocomplete(end)
+
+
+// Watch Submit
+
+function watchFormSubmit(){
+  $("#js-form-submit").on("click", function(event){
+    event.preventDefault();
+    getDestinations();
+    updateLegDataGeocode();
+    console.log(myGeoArray)
+  });
 }
 
-function displayEventBriteData(data){
-	data.events.forEach(function(item){
-		console.log(item);
-	})
-}
-
-function logEventBriteData(){
-	$("#js-form-submit").on("click", function(event){
-		getDataFromEventBrite(displayEventBriteData);
-	})
-}
-
-$(function(){ //ready function
-	addDest();
-	routeDest();
-	logEventBriteData();
-});
+$(function(){
+  watchFormSubmit();
+  addDest();
+})
