e5a306f4da478c8ad71b94bdbc33eeda0983d097
Matthew Peebles <peebles.mm@gmail.com>
2017-05-07 10:51:10 -0500

    Added state functionality to calculate dates
    Several functions were added in this update primarily focusing on
    calculating dates for the roadtrip for use in the eventbrite API.
    The eventbrite api's framework was added and prints out to console
    to verify it's working. Primarily, getLeg function now updates
    legDates variable with the dates of the leg. The index corresponds
    so legDates[0] is the date of leg[0]. legDates variable was also
    initialized.
    
    Additionally, getDates function was created. This function calculates
    all dates for the journey and pushes them to the dates array. It bases
    it's calculations off of the first date the user inputs and all the
    subsequent lengths of stay. Detailed comments are within the getDates
    function.

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+'use strict';
+
 // State
 var destinations = [];
+
+var dates = [];
 
 var addDestCounter = 1;
 
 var leg = [];
+
+var legDates = [];
 
 var autocompleteArray = [];
 
@@ -22,11 +28,11 @@ var legData  = {startPoint: {geocode: {latitude: null,
                           weather: {}
                           },
 
-               "0.5point": {geocode: {latitude: null,
+               "point0.5": {geocode: {latitude: null,
                                     longitude: null},
                           weather: {}
                          },
-               "1.5point": {geocode: {latitude: null,
+               "point1.5": {geocode: {latitude: null,
                                     longitude: null},
                           weather: {}
                          },
@@ -40,15 +46,35 @@ function getDestinations(){
   destinations = [];
   $("#destination-form .js-destination").each(function(){
     destinations.push($(this).val());
-  })
-  getLeg(); //initiates initial leg of the journey
+  }); //initiates initial leg of the journey
 }
 
-  //this function gets the leg of the journey.
+
+	// grabs first date and based on that calculates all
+	//subsequent dates based on length of time user inputs
+function getDates(){
+	dates = [];
+	var datesIndex = 0;
+	var startDate = document.getElementById("start-date").value;
+	dates.push(startDate);
+	$("#destination-form .length").each(function(){
+		var dtstr = dates[datesIndex]; //grabs last date in the array to calculate next date
+		var timeDate = new Date(dtstr.split("-").join("-")).getTime(); //this gets the last date's exact time in seconds
+		var length = $(this).val() //this grabs the length the user inputted
+		var newDate = new Date(timeDate+(length*24*60*60*1000)) //this calculates the new date based on the old date's exact time by adding the number of seconds in the length
+		var isoDate = newDate.toISOString(); //this converts it to ISO8601 as eventbrite requires
+		var localDate = isoDate.split('T')[0] //this removes the UTC timezone, will add it in the api call back manually
+		dates.push(localDate); //this pushes the calculated date in the yyyy-mm-dd format
+		datesIndex++; //this increments the dates index so the most recently pushed date becomes the last one to calculate the next leg.
+	})
+}
+
+  //this function gets the leg of the journey and the dates associated with it.
   //it will always be two destinations
   //this will be called once we start navigating
 function getLeg(){
   leg = [destinations[legCounter], destinations[legCounter + 1]];
+  legDates = [dates[legCounter], dates[legCounter +1]];
 }
 
     //Obtaining geocodes section
@@ -85,13 +111,45 @@ function addDest(){
   $("#destination-form").on("click", "#js-addDest", function(event){
     event.preventDefault();
     var newDestForm = "<br><input type=\"text\" name=\"dest-" + addDestCounter + "\" id = \"destination-" + addDestCounter + "\" class=\"js-destination\" placeholder=\"Destination\">" + 
-    "<input type=\"text\" name=\"Dest-" +addDestCounter+ "-date\" placeholder=\"length of stay\">" + 
+    "<input type=\"text\" name=\"Dest-" +addDestCounter+ "-date\" class=\"length\" placeholder=\"length of stay\">" + 
     "<button id=\"js-addDest\">Add Destination</button>";
     $(this).after(newDestForm);
-    addAutoComplete();
-    addDestCounter++;
+		// adds autocomplete functionality to each new input
+	var nPoint = "destination-" + addDestCounter;
+	var newPoint = document.getElementById(nPoint);
+	var autocompleteN = new google.maps.places.Autocomplete(newPoint);
+	addDestCounter++;
   })
 }
+
+
+// API SECTION
+
+    	//Obtaining Events for EndPoint
+	// ******************************************************************
+	var EVENTBRITE_BASE_URL = "https://www.eventbriteapi.com/v3/events/search/"
+
+	function getDataFromEventBrite(callback){
+		var query = {
+			"location.latitude": legData.endPoint.geocode.lat,
+			"location.longitude": legData.endPoint.geocode.lng,
+			"start_date.range_start": $("#start-date").val() + "T00:00:00",
+			// "start_data.range-end": 
+			token: "NYUIK7WAP7JD57IF4W4H",
+		}
+		$.getJSON(EVENTBRITE_BASE_URL, query, callback)
+	}
+
+	function displayEventBriteData(data){
+		data.events.forEach(function(item){
+			console.log(item);
+		})
+	}
+
+	function logEventBriteData(){
+		getDataFromEventBrite(displayEventBriteData);
+	}
+	// ******************************************************************
 
   //Google Autocomplete API Section
   //NEED TO DETERMINE HOW TO ADD FUNCTIONALITY
@@ -109,8 +167,10 @@ function watchFormSubmit(){
   $("#js-form-submit").on("click", function(event){
     event.preventDefault();
     getDestinations();
+    getDates();
+    getLeg();
     updateLegDataGeocode();
-    console.log(myGeoArray)
+    logEventBriteData();
   });
 }
 
